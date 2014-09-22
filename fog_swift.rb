7dda3d760254bc5e6ed6037eb04045e18a8fbe54
GarfieldLinux <garfieldlinux@gmail.com>
2014-09-22 17:20:42 +0800

    swift fog class testing

--- fog_swift.rb
+++ fog_swift.rb
@@ -1,11 +1,8 @@
 require 'fog'
 
-
 class FogSwift
-      
    @service  = ''
    @container_name = ''
-
     
    def initialize(_username,_appuser,_apppasswd,_authurl)
       begin
@@ -22,74 +19,74 @@ class FogSwift
       end
    end
 
-def show_service_requests
-   puts @service.requests.to_s
-end
-
-def show_service_head_containers
-   response = @service.head_containers
-   puts response.to_s
-   #response.status  
-   puts response.headers.to_s
-   puts response.status.to_s
-   puts response.body.to_s
-end
-
-def show_all_containers 
-   #show all the containers under user 
-   puts @service.directories.to_s
-end
-
-def create_container()
-   #create container
-   puts @service.directories.create(:key => @container_name)
-end
-
-def get_container()
-   #get_container
-   puts @service.directories.get(@container_name).to_s
-end
-
-def show_container_files()
-   #show container files
-   puts "get all the files under one container " 
-   puts @service.directories.get(@container_name).files
-end
-
-def upload_container_file(_file_name,_source_file_path,_file_data)
-   if (_source_file_path != '') && (_file_data =='') then
-   #upload file under one container  data from file on disk
-      file = @service.directories.get(@container_name).files.create(:key => _file_name, :body => File.open(_source_file_path))
-   elsif (_source_file_path == '') && (_file_data !='') then
-   #upload file under one container  data from file in mem 
-      file = @service.directories.get(@container_name).files.create(:key => _file_name, :body => _file_data)
-   else
-      puts "wrong number of arguments"
-      return -1
-      exit
-   end   
-end
-
-def download_contain_file(_swift_filename,_download_filename)
-   #To get one file and rename  
-   File.open(_download_filename, 'w') do | f |
-      @service.directories.get(@container_name).files.get(_swift_filename) do | data, remaining, content_length |
-         f.syswrite data
-      end
+   def show_service_requests
+      puts @service.requests.to_s
    end
-   #If a file object has already been loaded into memory, you can save it as follows:
-   #File.open('germany.jpg', 'w') {|f| f.write(file_object.body) }
-end
 
-def get_file_message(_filename)
-   #To get one file 
-   puts @service.directories.get(@container_name).files.get(_filename).to_s
-end
+   def show_service_head_containers
+      response = @service.head_containers
+      puts response.to_s
+      #response.status  
+      puts response.headers.to_s
+      puts response.status.to_s
+      puts response.body.to_s
+   end
 
-def delete_container_file(_swift_filename)
-   #To delete a file:
-   puts  @service.directories.get(@container_name).files.get(_swift_filename).destroy
-end
+   def show_all_containers 
+      #show all the containers under user 
+      puts @service.directories.to_s
+   end
+
+   def create_container()
+      #create container
+      puts @service.directories.create(:key => @container_name)
+   end
+
+   def get_container()
+      #get_container
+      puts @service.directories.get(@container_name).to_s
+   end
+
+   def show_container_files()
+      #show container files
+      puts "get all the files under one container " 
+      puts @service.directories.get(@container_name).files
+   end
+
+   def upload_container_file(_file_name,_source_file_path,_file_data)
+      if (_source_file_path != '') && (_file_data =='') then
+         #upload file under one container  data from file on disk
+         file = @service.directories.get(@container_name).files.create(:key => _file_name, :body => File.open(_source_file_path))
+      elsif (_source_file_path == '') && (_file_data !='') then
+         #upload file under one container  data from file in mem 
+         file = @service.directories.get(@container_name).files.create(:key => _file_name, :body => _file_data)
+      else
+         puts "wrong number of arguments"
+         return -1
+         exit
+      end   
+   end
+
+   def download_contain_file(_swift_filename,_download_filename)
+      #To get one file and rename  
+      File.open(_download_filename, 'w') do | f |
+         @service.directories.get(@container_name).files.get(_swift_filename) do | data, remaining, content_length |
+            f.syswrite data
+         end
+      end
+      #If a file object has already been loaded into memory, you can save it as follows:
+      #File.open('germany.jpg', 'w') {|f| f.write(file_object.body) }
+   end
+
+   def get_file_message(_filename)
+      #To get one file 
+      puts @service.directories.get(@container_name).files.get(_filename).to_s
+   end
+
+   def delete_container_file(_swift_filename)
+      #To delete a file:
+      puts  @service.directories.get(@container_name).files.get(_swift_filename).destroy
+   end
 
 end
 
